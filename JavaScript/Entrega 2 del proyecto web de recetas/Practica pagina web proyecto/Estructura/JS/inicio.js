c0f45c935843f88ee14617c7efd33dcfb2efa00c
FUNGILA-VÖCTORP\Mumponet2006 <victoraracil55@gmail.com>
2025-02-26 08:58:14 +0100

    no se

--- JavaScript/Entrega 2 del proyecto web de recetas/Practica pagina web proyecto/Estructura/JS/inicio.js
+++ JavaScript/Entrega 2 del proyecto web de recetas/Practica pagina web proyecto/Estructura/JS/inicio.js
@@ -1,14 +1,23 @@
+let lista = document.getElementsByClassName("tutorial");
+let items = lista.getElementsByTagName("article");
+alert(items.length);    // 3
+for(let i = 0; i < items.length; i++)
+{
+    // Uno, Dos, Tres, respectivamente    
+    alert(consejos[i].textContent); 
+}
+// Ruta hasta imagen "imagen.png"
+alert(imagen.src);
+
 let consejos = [
     {
         titulo: "Cómo evitar vulnerabilidades",
-        imagen: "consejos/consejo1.jpg",
         resumen: "Un consejo rapido para evitar brechas de seguridad en tu equipo.",
         valoracion: 10,
         efectividad: 7,
     },
     {
         titulo: "Gestiona tus contraseñas de forma segura",
-        imagen: "consejos/consejo2.jpg",
         resumen: "Consejos para mantener tus cuentas protegidas.",
         valoracion: 9,
         efectividad: 8,  
--- JavaScript/Entrega 2 del proyecto web de recetas/Practica pagina web proyecto/Estructura/JS/register.js
+++ JavaScript/Entrega 2 del proyecto web de recetas/Practica pagina web proyecto/Estructura/JS/register.js
@@ -1,38 +1,36 @@
-let pass = document.getElementById("password");
-let pass2 = document.getElementById("password2");
+function validarEmail(email) 
+	{
+	    var re = /\S+@\S+\.\S+/;
+	    return re.test(email);
+	}
+		var form = document.procesar;
 
-pass.addEventListener("invalid", function()
-{
-    if(pass.validity.valueMissing)
-    {
-        pass.setCustomValidity("El password no puede estar vacío");
-    }
-    else if (pass.validity.patternMismatch)
-    {
-        pass.setCustomValidity("El password debe tener al menos 8 caracteres, y sólo pueden ser letras y números");
-    }
-});
-pass.addEventListener("input", function()
-{
-    pass.setCustomValidity("");
-    if (pass.value != pass2.value)
-    {
-        pass2.setCustomValidity("Los passwords no coinciden");
-    }
-    else
-    {
-        pass2.setCustomValidity("");
-    }
-});
+	document.procesar.onsubmit = function(e){
+		var ready = false;
+		
+		if(form.nombre.value!="" && form.apellidos.value!=""&& form.email.value!=""&& form.password.value!="" && form.confirm.value!=""){
+			ready = true;
+		}else{
+				ready= false;
+				alert("Hay algunos campos vacios");
+				e.preventDefault();			
+		}
 
-pass2.addEventListener("input", function()
-{
-    if (pass.value != pass2.value)
-    {
-        pass2.setCustomValidity("Los passwords no coinciden");
-    }
-    else
-    {
-        pass2.setCustomValidity("");
-    }
-});
+		if(ready){
+			if(validarEmail(form.email.value)){
+				if(form.password.value==form.confirm.value){
+					ready = true;
+				}else{
+					ready= false;
+					alert("El password y la confirmacion no coinciden");
+					form.password.focus();
+					e.preventDefault();
+				}		
+			}else {
+					alert("El email no tiene un formato valido!");
+					form.email.focus();
+					e.preventDefault();			
+			}
+		}
+
+	}
